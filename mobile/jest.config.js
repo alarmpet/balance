265323fc63e27c71395fca360acc7b01b988c049
alarmpet <petblo12@gmail.com>
2026-07-15 05:03:12 +0900

    test(web): stub global css in jest

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   preset: 'jest-expo',
+  moduleNameMapper: {
+    '\\.(css)$': '<rootDir>/__mocks__/styleMock.js',
+  },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testMatch: ['**/__tests__/**/*.test.(ts|tsx)'],
 };
